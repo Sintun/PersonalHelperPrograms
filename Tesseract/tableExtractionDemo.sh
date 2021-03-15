e62841d41f07ba8f896ed394288c178b52f43a39
Stefan Brechtken <sb@ico-lux.de>
2021-03-15 17:22:30 +0100

    removing valgrind from the shell script, there are no leaks

--- Tesseract/tableExtractionDemo.sh
+++ Tesseract/tableExtractionDemo.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH
 # $home/BSC/OpenSourceDev/Tesseract/tesseract_build/bin/tesseract -l normal/deu "$1" test1 segdemo inter
 # SCROLLVIEW_PATH="$home/BSC/OpenSourceDev/Tesseract/tesseract_build/java/"
 # export SCROLLVIEW_PATH
-exec valgrind ./tableExtractionDemo -f "$1" -d "$2" -l $3
+exec ./tableExtractionDemo -f "$1" -d "$2" -l $3
 
 # ~/BSC/OpenSourceDev/Tesseract/tesseract_build/java$ java -jar ./ScrollView.jar
 # ./tess.sh ./tab1.tif /usr/share/icolux/tessdata/normal/ deu
